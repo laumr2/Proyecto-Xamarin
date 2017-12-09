e18df40968a4686b9a990afdeb1adf5dfeda3e72
Arnold Ulate <aulate@microsistemas.local>
2017-12-09 09:10:47 -0600

    Desde maquina

---
+++ ProyectoXamarinGrupo1/ProyectoXamarinGrupo1/View/FormularioView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoXamarinGrupo1.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FormularioView : ContentPage
+	{
+		public FormularioView ()
+		{
+			InitializeComponent ();
+
+            txtArticulo.Focused += TxtArticulo_Focused;
+
+        }
+
+        private void TxtArticulo_Focused(object sender, FocusEventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail.Navigation.PushAsync(new PopArticuloView());
+        }
+    }
+}
---
+++ ProyectoXamarinGrupo1/ProyectoXamarinGrupo1/View/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoXamarinGrupo1.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginView : ContentPage
+	{
+		public LoginView ()
+		{
+			InitializeComponent ();
+
+            btnIngresar.Clicked += BtnIngresar_Clicked;
+		}
+
+        private void BtnIngresar_Clicked(object sender, EventArgs e)
+        {
+            NavigationPage navigation = new NavigationPage(new PrincipalView());
+
+            App.Current.MainPage = new MasterDetailPage
+            {
+                Master = new MenuView(),
+                Detail = navigation
+            };
+        }
+    }
+}
